9013027ba50053c85c237cc7c26bff581899eab4
Alex Mitchell <alex@alexthemitchell.com>
2016-07-02 09:35:18 -0700

    Add project file  * Closes #1

---
+++ Publications/Publications/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Publications
+//
+//  Created by Alex Mitchell on 7/2/16.
+//  Copyright © 2016 Grinnell AppDev. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
