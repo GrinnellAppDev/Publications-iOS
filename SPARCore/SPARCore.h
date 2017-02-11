fcaf341f2b3a4e4d7f51d002ae36246326ab68af
Alex Mitchell <alex@alexthemitchell.com>
2017-02-11 10:40:46 -0600

    Add SPARCore target

---
+++ SPARCore/SPARCore.h
@@ -0,0 +1,19 @@
+//
+//  SPARCore.h
+//  SPARCore
+//
+//  Created by Alex Mitchell on 2/11/17.
+//  Copyright © 2017 comp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SPARCore.
+FOUNDATION_EXPORT double SPARCoreVersionNumber;
+
+//! Project version string for SPARCore.
+FOUNDATION_EXPORT const unsigned char SPARCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SPARCore/PublicHeader.h>
+
+
