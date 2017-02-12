811837fe0dc148dd1fcc97b0a3804715bc61dc33
Alex French <frenchal17@grinnell.edu>
2017-02-12 15:50:13 -0600

    Finish dummy loadArticles class method

--- SPARCore/GADArticle.m
+++ SPARCore/GADArticle.m
@@ -36,7 +36,23 @@
 
 + (NSArray <GADArticle *> *) loadArticles {
 
+    GADArticle *article = [[GADArticle alloc] init];
+    article.publicationId = @"8e031545-ba66-11e6-8193-a0999b05c023";
+    article.title = @"Testarticle";
+    article.authors = [NSArray arrayWithObjects:@{@"name": @"Addi", @"email": @"addisemail.edu"}, nil];
+    article.series = @"ada28c7d-a49f-11e6-b9d3-a0999b05c023";
+    article.url = [NSURL URLWithString:@"http://www.thesandb.com/news/shacs-to-offer-funds-for-students-in-need.html"];
+    article.tags = [NSArray arrayWithObjects:@"amazing!", @"fantastic!", @"Computer science!", nil];
+    article.brief = @"This is very brief";
+    article.content = @"This is much less brief because it should be longer than the brief";
+    article.datePublished = [NSDate date];
+    article.issue = @"ae2f3b54-a49f-11e6-b1ab-a0999b05c023";
+    article.id = @"db7bc676-ba7d-11e6-8de8-a20f19d048b3";
+    
+
+    
     NSArray <GADArticle *> *articleArray;
+    [articleArray arrayByAddingObject:article];
     
     return articleArray;
     
