d2bea71e7c371dae8f7d9eea35f541787fec2307
Alex French <frenchal17@grinnell.edu>
2017-02-12 14:55:56 -0600

    Partially stubbed out class/instance methods

--- SPARCore/GADArticle.h
+++ SPARCore/GADArticle.h
@@ -10,4 +10,18 @@
 
 @interface GADArticle : NSObject
 
+@property (nonatomic, strong) NSString *publicationId;
+@property (nonatomic, strong) NSArray *authors;
+@property (nonatomic, strong) NSURL *url;
+//@property (nonatomic, strong) NSInteger *pictureSize;
+@property (nonatomic, strong) NSString *series;
+@property (nonatomic, strong) NSArray *tags;
+@property (nonatomic, strong) NSString *brief;
+@property (nonatomic, strong) NSDate *datePublished;
+@property (nonatomic, strong) NSString *content;
+//@property (nonatomic, strong) NSSOMETHING *headerImage;
+@property (nonatomic, strong) NSString *title;
+@property (nonatomic, strong) NSString *issue;
+@property (nonatomic, strong) NSString *id;
+
 @end
--- SPARCore/GADArticle.m
+++ SPARCore/GADArticle.m
@@ -10,4 +10,37 @@
 
 @implementation GADArticle
 
+//- (Array of GADArticle) populateArticles: (NSString* publicationId) { } //pid is going to be S&B
+
+//-
+
+- (GADArticle *) initWithDictionary: (NSDictionary*) dict {
+    
+    GADArticle *article = [[GADArticle alloc] init];
+ 
+    article.publicationId = [dict valueForKey:@"publication"];
+    article.authors = [dict valueForKey:@"authors"];
+    //article.url = [dict valueForKey:@"publication"];
+    //article.pictureSize = [dict valueForKey:@"publication"];
+    //TODO: Finish property filling
+    return article;
+    
+}
+
+- (GADArticle *) updateArticleWithContent: (GADArticle*) articleStub {
+    
+    //DO the thing, add the stuff
+    
+    return articleStub;
+}
+
++ (NSArray <GADArticle *> *) loadArticles {
+
+    NSArray <GADArticle *> *articleArray;
+    
+    return articleArray;
+    
+}
+
+
 @end
