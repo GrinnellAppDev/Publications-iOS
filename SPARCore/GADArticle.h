e24326255caa07b74a93971f7b17800c80e24c8e
Alex French <frenchal17@grinnell.edu>
2017-02-12 13:44:37 -0600

    Add article model files

---
+++ SPARCore/GADArticle.h
@@ -0,0 +1,13 @@
+//
+//  GADArticle.h
+//  Publications
+//
+//  Created by Alex French on 2/12/17.
+//  Copyright © 2017 comp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GADArticle : NSObject
+
+@end
---
+++ SPARCore/GADArticle.m
@@ -0,0 +1,13 @@
+//
+//  GADArticle.m
+//  Publications
+//
+//  Created by Alex French on 2/12/17.
+//  Copyright © 2017 comp. All rights reserved.
+//
+
+#import "GADArticle.h"
+
+@implementation GADArticle
+
+@end
