aaf9c3fdee7ca4ce1eb9066b6e208ef5f3c8e2af
Alex French <frenchal17@grinnell.edu>
2017-03-01 21:25:27 -0600

    Progress on data stub query

--- SPARCore/GADArticle.m
+++ SPARCore/GADArticle.m
@@ -2,6 +2,31 @@
 
 @implementation GADArticle
 
+static const NSString *apiURL = @"https://g2j7qs2xs7.execute-api.us-west-2.amazonaws.com/devstable/publications";
+const NSTimeInterval timeoutInterval = 60.0;
+
++ (void) fetchArticlesFromPublication: (NSString *)publicationId completionHandler:(void(^_Nonnull)(NSData*))completion {
+    NSMutableArray *queryItems = [NSMutableArray<NSURLQueryItem *> new];
+    [queryItems addObject:[NSURLQueryItem queryItemWithName:@"publication" value:publicationId]];
+    
+    NSURLComponents *components = [NSURLComponents componentsWithString:apiURL];
+    components.queryItems = queryItems;
+    NSURL *url = components.URL;
+    
+    NSMutableURLRequest *req = [NSMutableURLRequest requestWithURL:url cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:timeoutInterval];
+    [req setHTTPMethod:@"POST"];
+    [req setValue:@"application/json" forHTTPHeaderField: @"Content-Type"];
+    
+    NSURLSessionDataTask *task = [[NSURLSession sharedSession]dataTaskWithRequest:req completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        if (error) {
+            NSLog(@"Error: %@", error);
+            return;
+        }
+        completion(data);
+    }];
+    [task resume];
+}
+
 + (NSArray <GADArticle *> *) articlesFromJSON: (NSData *)json {
     
     NSMutableArray <GADArticle *> *articles = [[NSMutableArray alloc] init];
